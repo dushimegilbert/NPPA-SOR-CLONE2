d3d23b3041a14c4caedd3f210c9ffe4af8be3f8a
Dushime Gilbert <dushimegilbert124@.com>
2024-11-19 13:46:37 +0200

    Second Commit

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  private apiUrl = "http://localhost:3002"
+  private reservations: Reservation[] = [];
+
+  constructor(private http: HttpClient){}
+
+  //CRUD
+
+  getReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(this.apiUrl + "/reservations");
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+
+    reservation.id = Date.now().toString();
+
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1)
+  }
+
+  updateReservation(id: string, updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations[index] = updatedReservation;
+  }
+  
+}
